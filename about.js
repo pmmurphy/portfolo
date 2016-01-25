4af8d3b525b5739bf81bba3a6db663b52148b7b3
Paula Murphy <paulam849@gmail.com>
2016-01-24 20:51:27 -0500

    added projects

---
+++ about.js
@@ -0,0 +1,18 @@
+$(document).ready(
+	function(){
+		$(".seuss").fadeIn("slow");
+});		
+
+// I would like to fadeIn my two paragraphs on the about page.
+// I will add an interest page
+
+// need to add image to contact page
+// need to fix pinterest icon
+// add images to interest page
+
+$(document).ready(
+	function(){
+		$(".seuss").delay(1500).fadeIn(5500);
+		$(".seuss").animate({color: green});
+
+	});
